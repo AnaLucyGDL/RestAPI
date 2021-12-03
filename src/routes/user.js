875ff1642c387c10e0a8cc36287594225ee945d8
Beatriz Martinez <trism@gmail.com>
2021-12-03 17:50:34 -0600

    Creation of a get petition

--- src/routes/user.js
+++ src/routes/user.js
@@ -7,9 +7,16 @@ const router = express.Router();
 // Create user
 router.post('/users', (req, res) => {
     const user = userSchema(req.body); // Create a user with the data from the request in body
-    console.log(req.body);
     user
         .save() // Save the user in the DB
+        .then((data) => res.json(data)) // What to do if it's correct
+        .catch((err) => res.json({ message: err }));
+});
+
+// Get all users
+router.get('/users', (req, res) => {
+    userSchema
+        .find()
         .then((data) => res.json(data)) // What to do if it's correct
         .catch((err) => res.json({ message: err }));
 });
