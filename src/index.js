7a0ad483101551528f005cdc1f56a4d0c8c43b36
Bety Martinez <trism460@gmail.com>
2021-12-03 12:47:43 -0600

    Message when enter to the main route in navigator

--- src/index.js
+++ src/index.js
@@ -6,4 +6,11 @@ const app = express();
 env('./.env');
 const port = process.env.PORT || 9001;
 
+// Routes
+
+// Main route of the app
+app.get('/', (req, res) => { // req - Request object / res - Response object
+    res.send('Welcome to my API');
+})
+
 app.listen(port, () => console.log(`Server working on port ${port}`));
