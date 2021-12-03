26a41056ac4a8a928794bc5e88b1660043de0c66
Beatriz Martinez <trism@gmail.com>
2021-12-03 16:45:22 -0600

    Creation of user model

---
+++ src/models/user.js
@@ -0,0 +1,19 @@
+// Create users model or schema
+const mongoose = require('mongoose');
+const userSchema = mongoose.Schema({
+    // Data that a user must have
+    name: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
